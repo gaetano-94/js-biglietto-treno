96a46b6d893230d8738bf26dc9c3ba7890fd375a
gaetano-94 <gaetano.panico1994@gmail.com>
2023-11-06 16:48:28 +0100

    feat: js eta passeggero

--- myscript.js
+++ myscript.js
@@ -11,7 +11,8 @@ const etaPasseggero = prompt('inserisci la tua età');
 /*Sulla base di queste informazioni dovrà calcolare il prezzo totale del biglietto di viaggio, secondo le seguenti regole:
 il prezzo del biglietto è definito in base ai km (0.21 € al km)*/
 
-//totale = km (da trasformare in un numero)prezzo (0.21)
+//totale = km (da trasformare in un numero) * prezzo (0.21)
+const totale = numeroChilometri * 0.21;
 
 /*va applicato uno sconto del 20% per i minorenni
 va applicato uno sconto del 40% per gli over 65.*/
@@ -20,9 +21,11 @@ va applicato uno sconto del 40% per gli over 65.*/
 
 //se l'età(da trasformare in un numero) e minore dei 18 anni
 //prezzo scontato (del 20%)
+const totaleSconto20 = etaPasseggero;
 
 //altrimenti se l'età(da trasformare in un numero) e superiore ai 65 anni
 //prezzo scontato (del 40%)
+const totaleSconto40 = etaPasseggero;
 
 /*L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
 Questo richiederà un minimo di ricerca*/
