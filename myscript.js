2f50336f9c598bc52e3088f3c32422900282c2a5
gaetano-94 <gaetano.panico1994@gmail.com>
2023-11-08 20:22:45 +0100

    feat: prezzokm piu sconti 20/40

--- myscript.js
+++ myscript.js
@@ -5,3 +5,14 @@ const kmRichiesti = Number(prompt('Inserisci i km che vuoi percorrere'));
 
 //eta inserita dall'utente
 const etaPasseggero = Number(prompt('Inserisci la tua età'));
+
+// creo una variabile per il prezzo del biglietto per km
+const prezzoKm = 0.21;
+
+/* creo due variabili per gli sconti del 20% e del 40% per il costro del biglietto */
+
+//creo una variabile per lo sconto minorenni del 20%
+const percentualeScontoMinorenni = 20;
+
+//creo una variabile per lo sconto over 65 del 40%
+const percentualeScontoOver65 = 40;
